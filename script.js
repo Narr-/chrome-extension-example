3abee78d692f9f4c5cc88b70628812e3e9b1bdcd
Narr <nardgu@gmail.com>
2015-11-16 09:06:02 +0900

    Check the window load status and if it is already loaded, run the script immediately. fix #1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.addEventListener('load', function() {
+function loadScript() {
   var s = document.createElement('script');
   s.src = chrome.extension.getURL('enable-selection.js');
   s.async = true;
@@ -8,4 +8,12 @@ window.addEventListener('load', function() {
   // this.parentNode.removeChild(this);
   // };
   (document.head || document.documentElement).appendChild(s);
-});
+}
+
+if (document.readyState === "complete") {
+  loadScript();
+} else {
+  window.addEventListener('load', function() {
+    loadScript();
+  });
+}
