c677f06d93a42564cad8791b23180837f3d9d2de
Narr <nardgu@gmail.com>
2016-02-20 01:14:55 +0900

    enable oncontextmenu event and change search api

--- popup.js
+++ popup.js
@@ -2,86 +2,65 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-(function() {
+(function(apiKey) {
   'use strict';
 
-  /**
-   * Get the current URL.
-   * @param {function(string)} callback - called when the URL of the current tab is found.
-   */
-  function getCurrentTabUrl(callback) {
-    // Query filter to be passed to chrome.tabs.query - see
-    // https://developer.chrome.com/extensions/tabs#method-query
-    var queryInfo = {
-      active: true,
-      currentWindow: true
-    };
+  if (!apiKey) {
+    console.error('no apikey..!!');
+    return;
+  }
 
-    chrome.tabs.query(queryInfo, function(tabs) {
-      // chrome.tabs.query invokes the callback with a list of tabs that match the
-      // query. When the popup is opened, there is certainly a window and at least
-      // one tab, so we can safely assume that |tabs| is a non-empty array.
-      // A window can only have one active tab at a time, so the array consists of
-      // exactly one tab.
-      var tab = tabs[0];
-
-      // A tab is a plain object that provides information about the tab.
-      // See https://developer.chrome.com/extensions/tabs#type-Tab
-      var url = tab.url;
-
-      // tab.url is only available if the "activeTab" permission is declared.
-      // If you want to see the URL of other tabs (e.g. after removing active:true
-      // from |queryInfo|), then the "tabs" permission is required to see their
-      // "url" properties.
-      console.assert(typeof url === 'string', 'tab.url should be a string');
-
-      callback(url);
-    });
-
-    // Most methods of the Chrome extension APIs are asynchronous.
-    // This means that you CANNOT do something like this:
-    //
-    // var url;
-    // chrome.tabs.query(queryInfo, function(tabs) {
-    //   url = tabs[0].url;
-    // });
-    // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+  function renderStatus(statusText) {
+    document.getElementById('status').textContent = statusText;
   }
 
   /**
-   * @param {string} searchTerm - Search term for Google Image search.
+   * @param {string} searchTerm - Search term
    * @param {function(string,number,number)} callback - Called when an image has
    *   been found. The callback gets the URL, width and height of the image.
    * @param {function(string)} errorCallback - Called when the image is not found.
    *   The callback gets a string that describes the failure reason.
    */
   function getImageUrl(searchTerm, callback, errorCallback) {
-    // Google image search - 100 searches per day.
-    // https://developers.google.com/image-search/
-    var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-      '?v=1.0&q=' + encodeURIComponent(searchTerm);
+    var searchUrl = 'https://www.googleapis.com/customsearch/v1?q=' + encodeURIComponent(searchTerm) +
+    '&cx=015734221451335448505%3Aogelzxkrimi&imgSize=medium&dateRestrict=d30&key=' + encodeURIComponent(apiKey);
+
     var x = new XMLHttpRequest();
     x.open('GET', searchUrl);
-    // The Google image search API responds with JSON, so let Chrome parse it.
     x.responseType = 'json';
     x.onload = function() {
-      // Parse and process the response from Google Image Search.
       var response = x.response;
-      console.log(response);
-      if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-        errorCallback('No response from Google Image search!');
+      // console.log(response);
+      if (!response || !response.items || response.items.length === 0) {
+        errorCallback('No response from yahoo..!!');
         return;
       }
-      var rstLen = response.responseData.results.length;
+
+      var images = [];
+      response.items.forEach(function(value, index) {
+        if(value.pagemap && value.pagemap.metatags && value.pagemap.metatags[0] &&
+          value.pagemap.metatags[0]['og:image']) {
+          images.push(value.pagemap.metatags[0]);
+        }
+      });
+
+      if (images.length < 1) {
+        errorCallback('No images from yahoo..!!');
+        return;
+      }
+
+      var rstLen = images.length;
       var index = Math.floor(Math.random() * rstLen);
-      var result = response.responseData.results[index];
-      var imageUrl = result.tbUrl;
-      var width = parseInt(result.tbWidth);
-      var height = parseInt(result.tbHeight);
+      var result = images[index];
+      // console.log(result);
+      var imageUrl = result['og:image'];
+      var width = parseInt(result['og:image:width']);
+      width = width > 400 ? 400 : width;
+      var height = parseInt(result['og:image:height']);
+      height = height > 250 ? 250 : height;
       console.assert(
         typeof imageUrl === 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected response from the Google Image Search API!');
+        'Unexpected response from yahoo..!!');
       callback(imageUrl, width, height);
     };
     x.onerror = function() {
@@ -90,41 +69,13 @@
     x.send();
   }
 
-  function renderStatus(statusText) {
-    document.getElementById('status').textContent = statusText;
-  }
-
   document.addEventListener('DOMContentLoaded', function() {
-    /*
-    getCurrentTabUrl(function(url) {
-      // Put the image URL in Google search.
-      renderStatus('Performing Google Image search for ' + url);
-
-      getImageUrl(url, function(imageUrl, width, height) {
-        renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-        var imageResult = document.getElementById('image-result');
-        // Explicitly set the width/height to minimize the number of reflows. For
-        // a single image, this does not matter, but if you're going to embed
-        // multiple external images in your page, then the absence of width/height
-        // attributes causes the popup to resize multiple times.
-        imageResult.width = width;
-        imageResult.height = height;
-        imageResult.src = imageUrl;
-        imageResult.hidden = false;
-
-      }, function(errorMessage) {
-        renderStatus('Cannot display image. ' + errorMessage);
-      });
-    });
-    */
-
-    var searchWord = 'Jessica Alba';
-    renderStatus('Performing Google Image search for ' + searchWord);
+    var searchWord = 'jessica alba';
+    renderStatus('Performing image search for ' + searchWord);
     getImageUrl(searchWord,
       function(imageUrl, width, height) { // success callback
         renderStatus('Search term: ' + searchWord + '\n' +
-          'Google image search result: ' + imageUrl);
+          'image search result: ' + imageUrl);
         var imageResult = document.getElementById('image-result');
         // Explicitly set the width/height to minimize the number of reflows. For
         // a single image, this does not matter, but if you're going to embed
@@ -133,10 +84,10 @@
         imageResult.width = width;
         imageResult.height = height;
         imageResult.src = imageUrl;
-        imageResult.style.display = 'block'; // to set margin:0 auto
+        imageResult.style.display = 'block';
       },
       function(errorMessage) { // error callback
         renderStatus('Cannot display image. ' + errorMessage);
       });
   });
-})();
+})(window.apiKey);
--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function loadScript() {
   var s = document.createElement('script');
-  s.src = chrome.extension.getURL('enable-selection.js');
+  s.src = chrome.extension.getURL('enable-mouse-event.js');
   s.async = true;
   // s.onload = function() {
   // this.parentNode.removeChild(this);
@@ -10,7 +10,7 @@ function loadScript() {
   (document.head || document.documentElement).appendChild(s);
 }
 
-if (document.readyState === "complete") {
+if (document.readyState === 'complete') {
   loadScript();
 } else {
   window.addEventListener('load', function() {
